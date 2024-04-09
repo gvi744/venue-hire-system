7d96a4d0c0b1ef8d0efa6040173952ed55c97d90
gvi744 <jchi648@aucklanduni.ac.nz>
2024-04-09 22:25:58 +1200

    Fixed general code style formatting by autoformatting with Google Java Format for VS Code

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -2,33 +2,32 @@ package nz.ac.auckland.se281;
 
 public class Venue {
 
-    private String venueName;
-    private String venueCode;
-    private int capacity;
-    private int hireFee;
+  private String venueName;
+  private String venueCode;
+  private int capacity;
+  private int hireFee;
 
-    public Venue(String VenueName, String VenueCode, int Capacity, int HireFee) {
-      this.venueName = VenueName;
-      this.venueCode = VenueCode;
-      this.capacity = Capacity;
-      this.hireFee = HireFee;
-    }
-
-    // Getters for the venue class
-    public String getName() {
-      return venueName;
-    }
-
-    public String getCode() {
-      return venueCode;
-    }
-
-    public int getCapacity() {
-      return capacity;
-    }
-
-    public int getHireFee() {
-      return hireFee;
-    }
-
+  public Venue(String VenueName, String VenueCode, int Capacity, int HireFee) {
+    this.venueName = VenueName;
+    this.venueCode = VenueCode;
+    this.capacity = Capacity;
+    this.hireFee = HireFee;
   }
+
+  // Getters for the venue class
+  public String getName() {
+    return venueName;
+  }
+
+  public String getCode() {
+    return venueCode;
+  }
+
+  public int getCapacity() {
+    return capacity;
+  }
+
+  public int getHireFee() {
+    return hireFee;
+  }
+}
